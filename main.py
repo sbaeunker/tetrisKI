68200d5c8f2ee04a802798471134785eed461fd1
sbaunker <stefan.baeunker@hs-bochum.de>
2018-11-09 03:50:37 -0500

    added gamefield

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ Created on Mon Nov  5 13:46:05 2018
 
 @author: linux
 """
-
+import numpy as np
 import pygame
-
+#import Spielfeld
  
 # Überprüfen, ob die optionalen Text- und Sound-Module geladen werden konnten.
 if not pygame.font: print('Fehler pygame.font Modul konnte nicht geladen werden!')
@@ -14,6 +14,17 @@ if not pygame.mixer: print('Fehler pygame.mixer Modul konnte nicht geladen werde
 
 SCREEN_HEIGHT=800
 SCREEN_WIDTH=600
+
+FIELD_OFFSETX=30
+FIELD_OFFSETY=60
+
+FIELD_BLOCKSIZE=20
+
+BORDERTHICKNESS=4
+LINETHICKNESS=2
+COLOR_BORDER= (255,255,255) #white
+COLOR_LINES= (188,188,188)
+
 
 def main():
     # Initialisieren aller Pygame-Module und    
@@ -30,7 +41,17 @@ def main():
 
     # Clock-Objekt erstellen, das wir benötigen, um die Framerate zu begrenzen.
     clock = pygame.time.Clock()
-
+    spielfeld= [[0]*18]*10
+    
+    #drawing:
+    screen.fill((0, 0, 0))
+    
+    for i in range(1,len(spielfeld)):
+        pygame.draw.lines(screen, COLOR_LINES, False, [(FIELD_OFFSETX+i*FIELD_BLOCKSIZE,FIELD_OFFSETY),(FIELD_OFFSETX+i*FIELD_BLOCKSIZE,FIELD_OFFSETY+FIELD_BLOCKSIZE*len(spielfeld[0]))], 1);
+    for i in range(1, len(spielfeld[0])):
+        pygame.draw.lines(screen, COLOR_LINES, False, [(FIELD_OFFSETX,FIELD_OFFSETY+i*FIELD_BLOCKSIZE),(FIELD_OFFSETX+FIELD_BLOCKSIZE*len(spielfeld),FIELD_OFFSETY+i*FIELD_BLOCKSIZE)], 1);
+    pygame.draw.rect(screen, COLOR_BORDER, (FIELD_OFFSETX,FIELD_OFFSETY,FIELD_BLOCKSIZE*len(spielfeld),FIELD_BLOCKSIZE*len(spielfeld[0])), BORDERTHICKNESS);
+    pygame.display.update()
     # Die Schleife, und damit unser Spiel, läuft solange running == True.
     running = True
 
@@ -39,7 +60,7 @@ def main():
         # Pygame wartet, falls das Programm schneller läuft.
         clock.tick(30)
         # screen-Surface mit Schwarz (RGB = 0, 0, 0) füllen.
-        screen.fill((0, 0, 0))
+        
 
         # Alle aufgelaufenen Events holen und abarbeiten.
         for event in pygame.event.get():
@@ -51,6 +72,9 @@ def main():
             if event.type == pygame.KEYDOWN:
                 # Wenn Escape gedrückt wird, posten wir ein QUIT-Event in Pygames Event-Warteschlange.
                 if event.key == pygame.K_ESCAPE:
+                    running = False
+                    pygame.display.quit()
+                    pygame.quit()
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
 
         # Inhalt von screen anzeigen.
