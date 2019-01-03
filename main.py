63278b4e07396e24fc5a9fb8dd68be80e37046d4
Stefan Baeunker <stefan.baeunker@hs-bochum.de>
2019-01-03 09:46:11 +0100

    mergecommit

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ Created on Thu Nov 22 11:20:10 2018
 import Game
 
 def main():
-    game = Game.Game(800,600)
+    game = Game.Game(800,600,0)
+    #mode 0 = playMode
+    #mode 1 = agentLearning ohne Grafik
+    #mode 2 = agentLearning mit Grafik
+    #mode 3 = debug
     
     # Überprüfen, ob dieses Modul als Programm läuft und nicht in einem anderen Modul importiert wird.
 if __name__ == '__main__':
     main()
+ 
